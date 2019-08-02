b926504bea9f4cc4d652aa8a7661b1ce16137b06
ChngYrNick <cakedispensers@gmail.com>
2019-08-02 17:42:07 +0300

    Initial commit

---
+++ config.js
@@ -0,0 +1,39 @@
+/////////////////////////////////////////////////////////////////////
+// Copyright (c) Autodesk, Inc. All rights reserved
+// Written by Forge Partner Development
+//
+// Permission to use, copy, modify, and distribute this software in
+// object code form for any purpose and without fee is hereby granted,
+// provided that the above copyright notice appears in all copies and
+// that both that copyright notice and the limited warranty and
+// restricted rights notice below appear in all supporting
+// documentation.
+//
+// AUTODESK PROVIDES THIS PROGRAM "AS IS" AND WITH ALL FAULTS.
+// AUTODESK SPECIFICALLY DISCLAIMS ANY IMPLIED WARRANTY OF
+// MERCHANTABILITY OR FITNESS FOR A PARTICULAR USE.  AUTODESK, INC.
+// DOES NOT WARRANT THAT THE OPERATION OF THE PROGRAM WILL BE
+// UNINTERRUPTED OR ERROR FREE.
+/////////////////////////////////////////////////////////////////////
+
+// Autodesk Forge configuration
+module.exports = {
+  // Set environment variables or hard-code here
+  credentials: {
+    client_id: "oT0JtgJAwHAhieTl9cB92fcMefePi2Gf",
+    client_secret: "7QjlyRIwqF9aoci2",
+    callback_url: "http://localhost:3000/api/forge/oauth/token"
+  },
+  scopes: {
+    // Required scopes for the server-side application
+    internal: [
+      "bucket:create",
+      "bucket:read",
+      "data:read",
+      "data:create",
+      "data:write"
+    ],
+    // Required scope for the client-side viewer
+    public: ["viewables:read"]
+  }
+};
